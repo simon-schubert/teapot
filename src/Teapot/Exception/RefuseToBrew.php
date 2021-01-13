222d6a2dccc9aa801eea965173ce9c12dbc7ea0a
Simon Schubert <si.schubert@live.de>
2021-01-13 23:23:08 +0100

    result + error handler

--- src/Teapot/Exception/RefuseToBrew.php
+++ src/Teapot/Exception/RefuseToBrew.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App\Teapot\Exception;
 
-final class RefuseToBrew extends \RuntimeException
+use App\Teapot\View\Refuse;
+use Symfony\Component\HttpFoundation\Response;
+use Violines\RestBundle\Error\ErrorInterface;
+
+final class RefuseToBrew extends \RuntimeException implements ErrorInterface
 {
     public static function coffee()
     {
         return new static('Refuse to brew coffee because I\'m a teapot, permanently.');
+    }
+
+    public function getContent(): object
+    {
+        return Refuse::fromException($this);
+    }
+
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_I_AM_A_TEAPOT;
     }
 }
