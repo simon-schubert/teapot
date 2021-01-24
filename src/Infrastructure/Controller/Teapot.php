0100f3ff180fca11c0761955099295e0fcad5e66
Simon Schubert <si.schubert@live.de>
2021-01-24 16:39:36 +0100

    use native violines response resolvers
    remove resolver aliases
    
    add new line config
    
    _
    
    _

--- src/Infrastructure/Controller/Teapot.php
+++ src/Infrastructure/Controller/Teapot.php
@@ -4,39 +4,39 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller;
 
-use App\Infrastructure\Response\ErrorHandler;
-use App\Infrastructure\Response\SuccessHandler;
 use App\Teapot\Command\CreateBeverages;
 use App\Teapot\TeapotAppService;
 use App\Teapot\View\Message;
 use App\Teapot\View\TeapotStatus;
 use Symfony\Component\HttpFoundation\Request;
 use Violines\RestBundle\Error\ErrorInterface;
+use Violines\RestBundle\Response\ErrorResponseResolver;
+use Violines\RestBundle\Response\SuccessResponseResolver;
 
 use function React\Promise\resolve;
 
 final class Teapot
 {
-    private ErrorHandler $errorHandler;
-    private SuccessHandler $successHandler;
+    private ErrorResponseResolver $errorResponseResolver;
+    private SuccessResponseResolver $successResponseResolver;
     private TeapotAppService $teapotAppService;
 
-    public function __construct(ErrorHandler $errorHandler, SuccessHandler $successHandler, TeapotAppService $teapotAppService)
+    public function __construct(ErrorResponseResolver $errorResponseResolver, SuccessResponseResolver $successResponseResolver, TeapotAppService $teapotAppService)
     {
-        $this->errorHandler = $errorHandler;
-        $this->successHandler = $successHandler;
+        $this->errorResponseResolver = $errorResponseResolver;
+        $this->successResponseResolver = $successResponseResolver;
         $this->teapotAppService = $teapotAppService;
     }
 
     public function hello(Request $request)
     {
-        return resolve($this->successHandler->handle(Message::hello(), $request));
+        return resolve($this->successResponseResolver->resolve(Message::hello(), $request));
     }
 
     public function brew(CreateBeverages $createBeverages, Request $request)
     {
         return $this->teapotAppService->brew($createBeverages)
-            ->then(fn (TeapotStatus $status) => $this->successHandler->handle($status, $request))
-            ->otherwise(fn (ErrorInterface $e) => $this->errorHandler->handle($e, $request));
+            ->then(fn (TeapotStatus $status) => $this->successResponseResolver->resolve($status, $request))
+            ->otherwise(fn (ErrorInterface $e) => $this->errorResponseResolver->resolve($e, $request));
     }
 }
